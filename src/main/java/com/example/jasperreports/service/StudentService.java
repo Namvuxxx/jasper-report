732f7c8d2ea821fa55d5f33aa979f7d810ce40a1
namvu1 <namvs@ominext.com>
2021-08-09 10:21:42 +0700

    2nd PR

---
+++ src/main/java/com/example/jasperreports/service/StudentService.java
@@ -0,0 +1,11 @@
+package com.example.jasperreports.service;
+
+import com.example.jasperreports.repository.StudentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StudentService {
+    private final StudentRepository repository;
+}
